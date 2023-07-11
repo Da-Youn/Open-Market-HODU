63d455dea28d7a827e4835bcf1c5f8cf6448f965
Da-Youn <dayoun202@gmail.com>
2023-07-11 23:17:53 +0900

    [🎨 Style] 홑따옴표로 변경

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './pages/Login';
+import Login from './pages/Login/Login';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
-import GlobalStyle from './GlobalStyle';
+import GlobalStyle from './styles/GlobalStyle';
 function App() {
-	return (
-		<>
-			<GlobalStyle />
-			<Router>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/accounts/login/" element={<Login />} />
-					<Route path="/accounts/cart/" element={<Cart />} />
-				</Routes>
-			</Router>
-		</>
-	);
+  return (
+    <>
+      <GlobalStyle />
+      <Router>
+        <Routes>
+          <Route path='/*' element={<Home />} />
+          <Route path='/accounts/login/*' element={<Login />} />
+          <Route path='/accounts/cart/*' element={<Cart />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 export default App;
